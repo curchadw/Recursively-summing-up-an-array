47ca78f7efad67987c17347fea0fbdffaec1590e
Curtis Laurence Chadwell <>
2021-04-11 01:35:57 +0000

    Edited some comments

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const sumArr = array =>{
   //if the array has no values it will return zero
   if (array.length === 0){
     return 0
-  //else we will start at first postition of the provided array and we will recursively work our way down the array with the slice method which is programatically reducing to one value
+  //else we will start at first postition of the provided array and we will recursively work our way down the array with the slice method which is programatically reducing the array to one value
   }else{
     return array[0] + sumArr(array.slice(1))
   }
